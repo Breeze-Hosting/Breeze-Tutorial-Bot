45b9fe301123ce5dad7e0483029020a08ae4f8c6
Ryan M <59982450+Ryanawesome111@users.noreply.github.com>
2025-04-19 11:42:46 -0400

    initial

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} and ready to respond to commands!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+if BOT_TOKEN:
+    bot.run(BOT_TOKEN)
+else:
+    print("Error: BOT_TOKEN is not set in the .env file.")
